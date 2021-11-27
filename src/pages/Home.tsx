a0dc718b76f4d27e39ce2a7ecf70c14587a9772d
jonatasandradedeveloper <jonatasandrade.develop@gmail.com>
2021-11-27 19:49:55 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, SafeAreaView } from 'react-native'
+
+export function Home() {
+    return (
+        <SafeAreaView style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+            <Text>Template React Native With Typescript</Text>
+        </SafeAreaView>
+    )
+}
